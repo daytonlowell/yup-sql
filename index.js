3f89182169fbca8969b1e62024c23a55ce60815f
Russ Bubley <russ.bubley@googlemail.com>
2023-08-01 13:27:09 +0100

    Enable optional port to be passed in

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const build = require(`./build`)
 const fetch = require(`./fetch`)
 
 module.exports = argv => {
-	const { schema, table, host, user, password, camel, connection } = {
+	const { schema, table, host, port, user, password, camel, connection } = {
 		host: `127.0.0.1`,
+		port: 3306,
 		user: `root`,
 		password: ``,
 		...argv,
@@ -16,6 +17,7 @@ module.exports = argv => {
 	} else {
 		const db = connection || mysql.createConnection({
 			host,
+			port,
 			user,
 			password,
 		})
